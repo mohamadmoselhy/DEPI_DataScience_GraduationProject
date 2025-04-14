97ad861a6d4a0affadc9837bab2bf46923aca854
Mohamed Ibrahim Moselhy <Mohamed_Moselhy@outlook.com>
2025-04-14 04:43:17 +0200

    start integrations

---
+++ Milestone 04/Deployment/001 📊 Data Description/Application.bat
@@ -0,0 +1,8 @@
+@echo off
+REM Set the Python executable path (adjust if needed)
+set PYTHON_PATH=python
+
+REM Run the Python script with the CSV path as an argument
+%PYTHON_PATH% Main.py "D:\My drive\Course\Data Science\Projects\Graduation Projects\DataAnalysis_DEPI_GraduationProject\old_GraduationProject\Dataset and code\DataSet\InitailDataSet\DataSet Before Cleanig.csv"
+
+pause
