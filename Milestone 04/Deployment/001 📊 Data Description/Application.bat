42874ee40dc203dcce8d4c93f1d3007280f48a39
Mohamed Ibrahim Moselhy <Mohamed_Moselhy@outlook.com>
2025-04-14 05:07:06 +0200

    Update config file

--- Milestone 04/Deployment/001 📊 Data Description/Application.bat
+++ Milestone 04/Deployment/001 📊 Data Description/Application.bat
@@ -3,6 +3,6 @@ REM Set the Python executable path (adjust if needed)
 set PYTHON_PATH=python
 
 REM Run the Python script with the CSV path as an argument
-%PYTHON_PATH% Main.py "D:\My drive\Course\Data Science\Projects\Graduation Projects\DataAnalysis_DEPI_GraduationProject\old_GraduationProject\Dataset and code\DataSet\InitailDataSet\DataSet Before Cleanig.csv"
+%PYTHON_PATH% Main.py 
 
 pause
--- Milestone 04/Deployment/001 📊 Data Description/Main.py
+++ Milestone 04/Deployment/001 📊 Data Description/Main.py
@@ -1,38 +1,11 @@
 from src.Data_Description import preprocess_and_generate_reports
 from src.Data_Preprocessing import preprocess_data
 import sys
-from MyDataUitlsLib import save_to_csv
+from src.config import columns_to_remove , encoding_config, csv_path
 
-def main(csvpath):
+def main(csv_path):
     # Run the preprocessing and generate reports for the given CSV file
-    df = preprocess_and_generate_reports(csvpath, output_folder="1-preprocess_and_generate_reports_1")
-
-    # Columns to remove
-    columns_to_remove = ['rownumber', 'customerid', 'surname']
-    
-    # Encoding configuration
-    encoding_config = {
-        "creditscore": {
-            "new_column": "creditscorerange",
-            "bins": [300, 580, 670, 740, 800, 851, float('inf')],
-            "labels": [0, 1, 2, 3, 4]
-        },
-        "balance": {
-            "new_column": "balancerange",
-            "bins": [0, 40000, 120000, float('inf')],
-            "labels": [0, 1, 2]
-        },
-        "estimatedsalary": {
-            "new_column": "estimatedsalaryrange",
-            "bins": [0, 40000, 70000, float('inf')],
-            "labels": [0, 1, 2]
-        },
-        "tenure": {
-            "new_column": "tenurerange",
-            "bins": [0, 2, 4, 7, float('inf')],
-            "labels": [0, 1, 2, 3]
-        }
-    }
+    df = preprocess_and_generate_reports(csv_path, output_folder="1-preprocess_and_generate_reports_1")
 
     # Preprocess the data and save the processed dataframe
     processed_df,ProcessedCSV = preprocess_data(df, columns_to_remove, encoding_config, output_folder="2-Data_Preprocessing")
@@ -42,11 +15,4 @@ def main(csvpath):
 
     
 
-
-if __name__ == "__main__":
-    # Ensure the CSV path is passed as a command line argument
-    if len(sys.argv) > 1:
-        csvpath = sys.argv[1]  # Get the CSV path from the command line argument
-        main(csvpath)
-    else:
-        print("Please provide the path to the CSV file.")
+main(csv_path)
